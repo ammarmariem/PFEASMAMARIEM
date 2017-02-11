89e67a475d62feaddd6c8114849a592387c3ae83
mariem ammar <mariem.ammar1992@hotmail.com>
2017-02-11 03:29:41 -0800

    connexion-inscription-gestionevenement

---
+++ UserProject/UserProject/Model/Evenement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserProject.Model
+{
+    public class Evenement
+
+    {
+        public String nom {get;set;}
+        public String description { get; set; }
+        public DateTime datetemp { get; set; }
+        public String duree { get; set; }
+        public String lieu { get; set; }
+        public String type { get; set; }
+        public int budget { get; set; }
+    }
+}
---
+++ UserProject/UserProject/Model/Utilisateur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserProject.Model
+{
+   public class Utilisateur
+    {
+        public String nom { get; set; }
+        public String prenom { get; set; }
+        public int telephone { get; set; }
+        public int cin { get; set; }
+        public DateTime dateNaissance { get; set; }
+        public String email { get; set; }
+        public String motDePasse { get; set; }
+        public  String confirmerMotPasse { get; set; }
+    }
+
+}
